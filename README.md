2e9419c9a68a635c97cf97d30fbe84897df9d271
Cheng Shao <terrorjack@type.dance>
2024-05-21 14:29:00 +0000

    240521

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Static builds of `wasm-tools`, `wasmtime-cli`, `wizer` for
 {x86_64,aarch64}-{linux,darwin,windows}:
 
-- `wasm-component-ld`: `v0.5.0`
-- `wasm-tools`: `v1.206.0`
-- `wasmtime-cli`: `v20.0.1`
-- `wit-bindgen-cli`: `v0.24.0`
+- `wasm-component-ld`: `v0.5.2`
+- `wasm-tools`: `v1.208.1`
+- `wasmtime-cli`: `v21.0.0`
+- `wit-bindgen-cli`: `v0.25.0`
 - `wizer`: `v6.0.0-git`
--- build.sh
+++ build.sh
@@ -4,21 +4,18 @@ set -eu
 
 cargo install \
   --all-features \
-  --config 'profile.release.lto = "thin"' \
   --config 'profile.release.strip = "symbols"' \
   --root "$PWD" \
-  --git https://github.com/type-dance/wizer.git \
+  --git https://github.com/TerrorJack/wizer.git \
   wizer ${1+"$@"}
 
 cargo install \
   --all-features \
-  --config 'profile.release.lto = "thin"' \
   --config 'profile.release.strip = "symbols"' \
   --root "$PWD" \
   wasm-component-ld wasm-tools wit-bindgen-cli ${1+"$@"}
 
 cargo install \
-  --features winch \
-  --config 'profile.release.lto = "thin"' \
+  --all-features \
   --root "$PWD" \
   wasmtime-cli ${1+"$@"}
