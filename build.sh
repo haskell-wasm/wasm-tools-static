97caf1a684234911516567f5afc74aeacdcf619e
Cheng Shao <terrorjack@type.dance>
2024-12-19 02:38:41 +0000

    250106

--- build.sh
+++ build.sh
@@ -4,18 +4,27 @@ set -eu
 
 cargo install \
   --all-features \
+  --config 'profile.release.lto = "thin"' \
   --config 'profile.release.strip = "symbols"' \
   --root "$PWD" \
   --git https://github.com/haskell-wasm/wizer.git \
   wizer ${1+"$@"}
 
 cargo install \
-  --all-features \
+  --config 'profile.release.lto = "thin"' \
   --config 'profile.release.strip = "symbols"' \
   --root "$PWD" \
-  wasm-component-ld wasm-tools wit-bindgen-cli ${1+"$@"}
+  wasm-tools ${1+"$@"}
 
 cargo install \
   --all-features \
+  --config 'profile.release.lto = "thin"' \
+  --config 'profile.release.strip = "symbols"' \
+  --root "$PWD" \
+  wasm-component-ld wit-bindgen-cli ${1+"$@"}
+
+cargo install \
+  --all-features \
+  --config 'profile.release.lto = "thin"' \
   --root "$PWD" \
   wasmtime-cli ${1+"$@"}
