8450f69aa8f3db74ad70bdfc90dda42068950cba
Cheng Shao <terrorjack@type.dance>
2024-12-19 02:38:41 +0000

    250106

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Static builds of `wasm-tools`, `wasmtime-cli`, `wizer` for
 {x86_64,aarch64}-{linux,darwin,windows}:
 
-- `wasm-component-ld`: `v0.5.10`
-- `wasm-tools`: `v1.220.0`
-- `wasmtime-cli`: `v27.0.0`
-- `wit-bindgen-cli`: `v0.35.0`
+- `wasm-component-ld`: `v0.5.11`
+- `wasm-tools`: `v1.222.0`
+- `wasmtime-cli`: `v28.0.0`
+- `wit-bindgen-cli`: `v0.36.0`
 - `wizer`: `v7.0.5-git`
--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -eu
 
 cargo install \
   --all-features \
+  --config 'profile.release.lto = "thin"' \
   --config 'profile.release.strip = "symbols"' \
   --root "$PWD" \
   --git https://github.com/haskell-wasm/wizer.git \
@@ -11,11 +12,13 @@ cargo install \
 
 cargo install \
   --all-features \
+  --config 'profile.release.lto = "thin"' \
   --config 'profile.release.strip = "symbols"' \
   --root "$PWD" \
   wasm-component-ld wasm-tools wit-bindgen-cli ${1+"$@"}
 
 cargo install \
   --all-features \
+  --config 'profile.release.lto = "thin"' \
   --root "$PWD" \
   wasmtime-cli ${1+"$@"}
